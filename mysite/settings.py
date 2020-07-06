d494188f426677a8b9ac4a0d3c1f89ea9de2bdc2
Jiwon <chloe326o.o@gmail.com>
2020-07-06 22:34:16 +0900

    secret key deleted from git file

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
-import os
+import os,json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,8 +19,23 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
+secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '18ehum%m^s)4jmz=k^6^4kxarn*d4)$w3y%jnpfux*%bqf1zn0'
+# SECRET_KEY = '18ehum%m^s)4jmz=k^6^4kxarn*d4)$w3y%jnpfux*%bqf1zn0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
